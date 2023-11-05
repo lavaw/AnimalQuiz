98b589efd5c312d270c91c5adfadb617f35bd7e4
Rudolf Amirian <ronaldo679@gmail.com>
2023-11-05 15:19:53 +0300

    added questions for app

--- AnimalQuiz/Models/Answer.swift
+++ AnimalQuiz/Models/Answer.swift
@@ -5,4 +5,28 @@
 //  Created by Rudolf Amiryan on 04.11.23.
 //
 
-import Foundation
+
+struct Answer {
+    let text: String
+    let type: AnimalType
+}
+
+enum AnimalType: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Собака"
+        case .cat:
+            return "Кошка"
+        case .rabbit:
+            return "Кролик"
+        case .turtle:
+            return "Черепаха"
+        }
+    }
+}
