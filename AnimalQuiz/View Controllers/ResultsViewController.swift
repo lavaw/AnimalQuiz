ef34f4cee69a6e1a7abd1b41d3a49b9e988f6302
Rudolf Amirian <ronaldo679@gmail.com>
2023-11-06 18:18:42 +0300

    add logic for resultVC

--- AnimalQuiz/View Controllers/ResultsViewController.swift
+++ AnimalQuiz/View Controllers/ResultsViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var animalTypeLabel: UILabel!
+    
+    var answers: [Answer]!
     // 1. Передать сюда массив с ответами
     // 2. Определить наиболее часто встречающийся тип животного
     // 3. Отобразить результаты на экране
@@ -16,6 +20,41 @@ class ResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
+        updateResult()
     }
 
+}
+
+extension ResultsViewController {
+    
+    private func updateResult() {
+        
+        var frequencyOfAnimals: [AnimalType: Int] = [:]
+        let animals = answers.map { $0.type }
+        
+        for animal in animals {
+            if let animalTypeCount = frequencyOfAnimals[animal] {
+                frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyOfAnimals[animal] = 1
+            }
+        }
+        
+        for animal in animals {
+            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+        }
+        
+        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value}
+        guard let mostFrequencyAnimal = sortedFrequencyOfAnimals.first?.key else {return}
+        
+        updateUI(with: mostFrequencyAnimal)
+    }
+    
+    private func updateUI(with animal: AnimalType?) {
+        animalTypeLabel.text = "Вы - \(animal?.rawValue ?? "🐶")!"
+        descriptionLabel.text = animal?.definition ?? ""
+    }
+    
 }
